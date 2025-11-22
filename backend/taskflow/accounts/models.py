52d0a276dc519a03cf6e199a47cbaf24849e3714
Sajjad-e-Ghasemi <sajjadeghasemii@gmail.com>
2025-11-22 22:58:13 +0330

    Test API

--- backend/taskflow/accounts/models.py
+++ backend/taskflow/accounts/models.py
@@ -2,19 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-
-class User(models.Model):
+class Account(models.Model):
     username = models.CharField(max_length=150, unique=True, verbose_name="نام کاربری")
     email = models.EmailField(unique=True, verbose_name="ایمیل")
     password = models.CharField(max_length=128, verbose_name="رمز عبور")
     date_joined = models.DateTimeField(auto_now_add=True, verbose_name="تاریخ عضویت")
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name="آواتار")
 
-
     def __str__(self):
         return f"{self.username} - {self.email}"
 
-    class Meta:
+    class Meta: 
         verbose_name = "کاربر"
         verbose_name_plural = "کاربران"
         ordering = ['-date_joined'] 
--- backend/taskflow/accounts/views.py
+++ backend/taskflow/accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class AccountView(APIView):
+    def get(self, request):
+        return Response({"message": "Hello, this is the Accounts view!"})
