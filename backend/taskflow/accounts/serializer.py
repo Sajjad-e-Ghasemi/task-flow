623b00a2cccea7f6532039072014d53af6a2ea18
sajjad-e-ghasemi <sajjadeghasemii@gmail.com>
2026-01-28 19:05:39 +0330

    Update serializer.py
    Fixed any little problems on accounts/serializer.py

--- backend/taskflow/accounts/serializer.py
+++ backend/taskflow/accounts/serializer.py
@@ -12,6 +12,6 @@ class AccountSerializer(ModelSerializer):
     def create(self, validated_data):
         user = Account.objects.create_user(
             username = validated_data['username'],
-            password = validated_data['password']
+            password = validated_data['password'],
         )
         return user
