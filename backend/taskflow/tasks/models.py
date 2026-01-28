00f674ee0f226dd8feed3b4877a03849ebed4705
sajjad-e-ghasemi <sajjadeghasemii@gmail.com>
2026-01-28 19:05:06 +0330

    Update models.py
    Add ownership on tasks/models.py

--- backend/taskflow/tasks/models.py
+++ backend/taskflow/tasks/models.py
@@ -1,14 +1,29 @@
 from django.db import models
 from django.utils import timezone
 from accounts.models import Account
+from django.conf import settings
 
 def in_30_days():
     pass
 
 class TaskModel(models.Model):
-    task_title = models.CharField(max_length=500, verbose_name="نام وظیفه")
-    task_desc = models.TextField(verbose_name="توضیحات وظایف")
-    task_author = models.ForeignKey(Account, on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    
+    task_title = models.CharField(
+        max_length=500, 
+        verbose_name="نام وظیفه"
+    )
+    task_desc = models.TextField(
+        verbose_name="توضیحات وظایف"
+    )
+
+    task_author = models.ForeignKey(
+        Account, 
+        on_delete=models.CASCADE
+    )
     task_started_at = models.DateTimeField(
         verbose_name="تاریخ شروع وظیفه",
         null=True,
@@ -18,7 +33,10 @@ class TaskModel(models.Model):
         verbose_name="تاریخ پایان وظیفه",
         null=True
     )
-    is_done = models.BooleanField(default=False, verbose_name="انجام شده/نشده")
+    is_done = models.BooleanField(
+        default=False, 
+        verbose_name="انجام شده/نشده"
+    )
 
     def __str__(self):
         return f"{self.task_title} / {self.is_done}"
