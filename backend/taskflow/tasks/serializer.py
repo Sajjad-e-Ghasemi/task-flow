79a8bbef0dd9565e8cb15241e7e3650a986b41b4
sajjad-e-ghasemi <sajjadeghasemii@gmail.com>
2026-01-29 14:54:39 +0330

    Update serializer.py
    Fix bugs on tasks/serializer.py

--- backend/taskflow/tasks/serializer.py
+++ backend/taskflow/tasks/serializer.py
@@ -6,12 +6,15 @@ class TaskSerializer(ModelSerializer):
     class Meta:
         model = TaskModel
         fields = '__all__'
+        read_only_field = ('owner', )
+        extra_kwargs = {
+            'owner': {'required': False}  
+        }
 
     def create(self, validated_data):
         task = TaskModel.objects.create(
             owner = self.context['request'].user,
             task_title = validated_data['task_title'],
             task_desc = validated_data['task_desc'],
-            task_author = validated_data['task_author']
         )
         return task
